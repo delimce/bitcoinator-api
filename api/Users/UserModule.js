6501782b98c4b499157cd49ab86a051f705fc449
Luis De Lima <delimce@gmail.com>
2018-01-07 23:25:29 -0400

    change some user routes

--- api/Users/UserModule.js
+++ api/Users/UserModule.js
@@ -2,7 +2,6 @@
 const conf = require("./config.json");
 const models = require("../../models");
 const Boom = require('boom');
-let resp = require("../../libs/RestHelper");
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
 let _ = require("lodash");
@@ -25,7 +24,7 @@ const userModule = {
                 config: { auth: conf.auth },
                 handler: async (request, h) => {
                     try {
-                        let result = models.User.findAll()
+                        let result = await models.User.findAll()
                         return result
                     } catch (err) {
                         return Boom.badImplementation('Failed to get....', err)
@@ -62,7 +61,6 @@ const userModule = {
                         if (_.isNull(result)) { ///doesn't exist
 
                             try {
-
                                 // you can also build, save and access the object with chaining:
                                 let newUser = models.User.build({ name: data.name, lastname: data.lastname, email: data.email, password: bcrypt.hashSync(data.password, 10) }).save()
                                 return newUser;
@@ -71,11 +69,8 @@ const userModule = {
                                 return Boom.badImplementation('Failed to get....', err)
                             }
 
-
-
                         } else { ///email exists
                             return Boom.badRequest(locale.getString("isRegistered"))
-
                         }
 
                     } catch (err) {
@@ -84,9 +79,126 @@ const userModule = {
 
 
                 }
+            },
+            {
+                method: 'POST',
+                path: conf.basePath + "/login",
+                config: {
+                    auth: conf.auth,
+                    validate: {
+                        payload: {
+                            email: Joi.string().email().required(),
+                            password: Joi.string().min(4).max(200).required()
+                        }
+                    }
+                },
+                handler: async (request, h) => {
+
+                    try {
+                        let data = request.payload;   // <-- this is the important line
+                        let result = await models.User.findOne({
+                            attributes: ['id', 'email', 'name', 'password', 'status'],
+                            where: { email: { [Op.eq]: data.email } }
+                        })
+
+                        // success
+                        if (!_.isNull(result)) {
+                            console.log("dos")
+                            if (bcrypt.compareSync(data.password, result.dataValues.password)) {
+                                // Passwords match
+                                if (result.dataValues.status) {
+
+                                    var obj = {}; // object/info you want to sign
+                                    obj.userId = result.dataValues.id;
+                                    obj.userName = result.dataValues.name;
+                                    obj.userEmail = result.dataValues.email;
+
+                                    let token = await utils.jwtEncodeObject(obj); //generating a new JWT
+                                    obj.token = token;
+
+                                    return obj;
+
+                                } else {
+                                    return Boom.badRequest(locale.getString("inactive"))
+                                }
+
+                            } else {
+                                // Passwords don't match
+                                return Boom.unauthorized(locale.getString("notLogged"));
+                            }
+
+                        } else {
+                            return Boom.unauthorized(locale.getString("notLogged"));
+                        }
+
+                    } catch (err) {
+                        return Boom.badImplementation('Failed to get....', err)
+                    }
+
+                }
+
+            },
+
+            {
+                method: 'PUT',
+                path: conf.basePath + "/resetPassword",
+                config: { auth: false },
+                handler: async (request, h) => {
+
+                    try {
+
+                        var data = request.payload   // <-- this is the important line
+
+                        let result = await models.User.findOne({
+                            attributes: ['id', 'email', 'name', 'lastname'],
+                            where: { email: { [Op.eq]: data.email } }
+                        })
+
+                        // success
+                        if (_.size(result) > 0) { /// user found
+                            let myData = {};
+                            myData.userId = result.dataValues.id;
+                            myData.email = result.dataValues.email;
+                            myData.fullname = result.dataValues.name + " " + result.dataValues.lastname;
+
+                            let myToken = utils.jwtEncodeObject(myData)
+                            return myToken;
+
+                        } else {
+                            return Boom.badRequest(locale.getString("isNotRegistered"))
+                        }
+
+                    } catch (err) {
+                        return Boom.badImplementation('Failed to get....', err)
+                    }
+                }
+            },
+
+            {
+                method: 'PUT',
+                path: conf.basePath + "/changePassord",
+                config: {
+                    auth: false,
+                    validate: {
+                        payload: {
+                            token: Joi.string().required(),
+                            password: Joi.string().min(4).max(200).required()
+                        }
+                    }
+                },
+                handler: async (request, h) => {
+
+                    var data = request.payload   // <-- this is the important line
+                    try {
+
+                     
+
+                    } catch (err) {
+                        return Boom.badImplementation('Failed to get....', err)
+                    }
+
+                }
             }
-
-
 
 
             // {
