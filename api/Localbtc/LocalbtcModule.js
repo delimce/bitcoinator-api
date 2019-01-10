f4c5fcb942ec116eac6e18cea10b9ceeebd92836
Luis De Lima <delimce@gmail.com>
2019-01-10 01:01:12 -0400

    localbitcoins sell by currency endpoint

--- api/Localbtc/LocalbtcModule.js
+++ api/Localbtc/LocalbtcModule.js
@@ -3,29 +3,28 @@ const conf = require("./config.json");
 const Boom = require('boom');
 const Joi = require('joi');
 const _ = require("lodash");
-let $localbtc = require("../../libs/LocalbitcoinsHelper");
+const localbtc = require("../../libs/LocalbitcoinsHelper");
 
 const localbtcModule = {
     register: async (server, options) => {
         server.route([
             {
                 method: 'get',
-                path: conf.basePath + "/getCoins",
+                path: conf.basePath + "/salesByCurrency/{id}",
                 config: {
                     auth: false
                 },
                 handler: async (request, h) => {
-
                     try {
-
-                      
+                         let resp = await localbtc.getSellByCurrency(request.params.id)
+                         return resp
 
                     } catch (err) {
                         return Boom.badImplementation('Failed to get....', err)
                     }
 
                 }
-            }
+            },
         ])
     },
     name: 'localbtcModule',
--- libs/LocalbitcoinsHelper.js
+++ libs/LocalbitcoinsHelper.js
@@ -3,9 +3,35 @@
 let requestify = require('requestify'); ///resource for execute vendor services
 let _ = require("lodash");
 
-exports.getAll = async function () {
+exports.getSellByCurrency = async function (currency) {
 
-    let coinMarketCap = await requestify.get('https://api.coinmarketcap.com/v1/ticker/');
-    return coinMarketCap.getBody()
+    let cur = String(currency);
+    let localbtc = await requestify.get('https://localbitcoins.com/sell-bitcoins-online/' + cur.toUpperCase() + '/.json');
+    let data = localbtc.getBody();
+    let pagination = data.pagination;
+
+    let list = [];
+    let results = [];
+
+    if (Number(data.data.ad_count) > 0) list = data.data.ad_list;
+
+    _.forEach(list, function (res) {
+        if (res.data.visible) {
+            let newSell = {}
+            newSell.profile = res.data.profile;
+            newSell.bank = res.data.bank_name;
+            newSell.msg = res.data.msg;
+            newSell.min = Number(res.data.min_amount);
+            newSell.max = (res.data.max_amount_available) ? Number(res.data.max_amount_available) : Number(res.data.max_amount);
+            newSell.price = res.data.temp_price,
+            newSell.location = res.data.location_string;
+            newSell.url = res.actions.public_view;
+            results.push(newSell)
+        }
+    });
+
+    return results;
+
+
 
 }
